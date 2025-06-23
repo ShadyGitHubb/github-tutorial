7f57fcc9604b7b95349eb142da93342a141aaac3
DerpynGamingJnR <rayp3012@gmail.com>
2025-06-23 14:05:23 +1200

    Music setup done (No music yet)

--- thelostlegacy/the-lost-legacy/scripts/title_screen.gd
+++ thelostlegacy/the-lost-legacy/scripts/title_screen.gd
@@ -5,14 +5,18 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
+# Bring the user from the title screen to the main game.
 func _start():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
+# Bring the user from the title screen to the credits page.
 func _credits():
 	get_tree().change_scene_to_file("res://scenes/credits-page.tscn")
 
+# Allow the user to quit and close the program.
 func _quit():
 	get_tree().quit()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
