2e98513fba11d594bb446208881d48942802b02a
ShadyGitHubb <shaansaxena2009@gmail.com>
2025-07-25 09:41:24 +1200

    Fixed Player's Movement and added animation to player

--- thelostlegacy/the-lost-legacy/scripts/player.gd
+++ thelostlegacy/the-lost-legacy/scripts/player.gd
@@ -4,6 +4,7 @@ var speed = 5.0
 var sensitivity = 0.003
 var look_up_limit = 80
 var look_down_limit = 80
+@onready var anim_player = $AnimationPlayer
 
 @onready var camera = $Camera3D
 
@@ -24,18 +25,33 @@ func _process(delta):
 
 	var direction = Vector3()
 
-	if Input.is_action_pressed("move_forward"):
+	if Input.is_action_just_pressed("move_forward"):
 		direction -= transform.basis.z
+		if Input.is_action_just_pressed("move_forward"):
+			anim_player.play("ArmatureAction")
+		elif Input.is_action_just_released("move_forward"):
+			anim_player.stop("ArmatureAction")
 		print("forwards")
+
 	if Input.is_action_pressed("move_backwards"):
 		direction += transform.basis.z
+		if Input.is_action_just_pressed("move_backwards"):
+			anim_player.play("ArmatureAction")
 		print("backwards")
+
 	if Input.is_action_pressed("move_left"):
 		direction -= transform.basis.x
+		if Input.is_action_just_pressed("move_left"):
+			anim_player.play("ArmatureAction")
 		print("left")
+
 	if Input.is_action_pressed("move_right"):
 		direction += transform.basis.x
-		print("backwards")
+		if Input.is_action_just_pressed("move_right"):
+			anim_player.play("ArmatureAction")
+		print("right")
+
+
 
 	direction = direction.normalized()
 
