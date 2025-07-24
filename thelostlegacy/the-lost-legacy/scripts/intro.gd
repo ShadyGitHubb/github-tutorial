92dec86a96c4a76ea540031ee5f79b8508127e01
DerpynGamingJnR <rayp3012@gmail.com>
2025-07-25 08:47:58 +1200

    Changed backdrop from blank to black

--- thelostlegacy/the-lost-legacy/scripts/intro.gd
+++ thelostlegacy/the-lost-legacy/scripts/intro.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("fade in")
-	await get_tree().create_timer(6).timeout
+	await get_tree().create_timer(4).timeout
 	$AnimationPlayer.play("fade out")
 	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://scenes/title-screen.tscn")
