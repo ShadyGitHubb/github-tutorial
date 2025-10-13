c9208ee142322b1682609063ad07d3960c5543e5
ShadyGitHubb <shaansaxena2009@gmail.com>
2025-10-13 13:44:51 +1300

    Fixed interaction with key

--- thelostlegacy/the-lost-legacy/scripts/title_screen.gd
+++ thelostlegacy/the-lost-legacy/scripts/title_screen.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 # Bring the user from the title screen to the main game.
 func _start():
