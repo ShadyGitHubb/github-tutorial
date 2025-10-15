c78eacdd8d0840c60faededac69d1e63e46e6e3e
DerpynGamingJnR <rayp3012@gmail.com>
2025-10-15 13:20:00 +1300

    Update pedestal.gd

--- thelostlegacy/the-lost-legacy/scenes/pedestal.gd
+++ thelostlegacy/the-lost-legacy/scenes/pedestal.gd
@@ -1,25 +1,31 @@
 extends Node3D
+
+const ANSWERS = ["128", "One hundred and twenty eight", "128 roses", "64 red, 64 black", "64, 64"]
+const TEXTURE_PATH = "res://Sprite3D"  # Replace with your actual PNG path
+
 @export var ui : Control
 @export var input_field : TextEdit  # Export a TextEdit node to get user input
-@export var roses_sprite : Sprite3D  # Export a Sprite2D to display the PNG
-
-var answers = ["128", "One hundred and twenty eight", "128 roses", "64 red, 64 black", "64, 64"]
+@export var roses_sprite : Sprite3D  # Export a Sprite3D to display the PNG
 
 func _ready() -> void:
 	ui.hide()
 
-
 func interact():
 	ui.show()
 
-
 func _answer() -> void:
-	if input_field.text in answers:
-		roses_sprite.texture = load("res://Sprite3D")
+	var user_text = input_field.text.strip_edges().to_lower()
+	
+	# Normalize answers for case-insensitive comparison
+	var normalized_answers = []
+	for ans in ANSWERS:
+		normalized_answers.append(ans.to_lower())
+	
+	if user_text in normalized_answers:
+		roses_sprite.texture = load(TEXTURE_PATH)
 		roses_sprite.show()
 	else:
 		roses_sprite.hide()
-
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
