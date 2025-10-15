d4a78b56c3d2f7ec64adae9e95ceafe4058b414d
DerpynGamingJnR <rayp3012@gmail.com>
2025-10-15 13:14:29 +1300

    Update intro.gd

--- thelostlegacy/the-lost-legacy/scripts/intro.gd
+++ thelostlegacy/the-lost-legacy/scripts/intro.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
+const FADE_IN_DURATION := 4.0
+const FADE_OUT_DURATION := 3.0
+const FADE_IN_ANIM := "fade in"
+const FADE_OUT_ANIM := "fade out"
+const NEXT_SCENE_PATH := "res://scenes/title-screen.tscn"
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimationPlayer.play("fade in")
-	await get_tree().create_timer(4).timeout
-	$AnimationPlayer.play("fade out")
-	await get_tree().create_timer(3).timeout
-	get_tree().change_scene_to_file("res://scenes/title-screen.tscn")
-pass
+    if $AnimationPlayer.has_animation(FADE_IN_ANIM) and $AnimationPlayer.has_animation(FADE_OUT_ANIM):
+        $AnimationPlayer.play(FADE_IN_ANIM)
+        await get_tree().create_timer(FADE_IN_DURATION).timeout
+        $AnimationPlayer.play(FADE_OUT_ANIM)
+        await get_tree().create_timer(FADE_OUT_DURATION).timeout
+        if ResourceLoader.exists(NEXT_SCENE_PATH):
+            get_tree().change_scene_to_file(NEXT_SCENE_PATH)
+        else:
+            push_error("Scene path does not exist: %s" % NEXT_SCENE_PATH)
+    else:
+        push_error("Required animations not found in AnimationPlayer")
